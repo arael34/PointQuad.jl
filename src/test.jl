c77b7692e4d7141a514c7a31e4a9e2b5c98cba64
rushia <jonasg.13452@gmail.com>
2022-02-04 23:52:39 -0500

    query function added

--- src/test.jl
+++ src/test.jl
@@ -2,7 +2,7 @@ using GameZero
 
 const WIDTH = 800
 const HEIGHT = 800
-const BACKGROUND = colorant"antiquewhite"
+const BACKGROUND = colorant"black"
 
 #=
 TODO
@@ -15,7 +15,7 @@ struct Point
     y::Real
 end
 
-struct Sqr
+mutable struct Sqr
     x::Real
     y::Real
     s::Real
@@ -70,6 +70,29 @@ function insert!(qt::QuadTree, p::Point)
     end
 end
 
+function intersects(A::Sqr, B::Sqr)
+    !(
+        A.x - A.s / 2 >= B.x + B.s / 2 ||
+        A.x + A.s / 2 <= B.x - B.s / 2 ||
+        A.y - A.s / 2 >= B.y + B.s / 2 ||
+        A.y + A.s / 2 <= B.y - B.s / 2
+    )
+end
+
+function query(qt::QuadTree, bounds::Sqr)
+    found = Vector{Point}()
+    if !intersects(qt.boundary, bounds) return found end
+    for p in qt.points
+        if !contains(bounds, p) continue end
+        push!(found, p)
+    end
+    if isleaf(qt) return found end
+    for section in qt.cr
+        append!(found, query(section, bounds))
+    end
+    return found
+end
+
 function generate_quad(qt::QuadTree)
     l = Vector{QuadTree}()
     push!(l, qt)
@@ -85,20 +108,32 @@ function generate_quad(qt::QuadTree)
 end
 
 pslol = Vector{Point}()
+foundp = Vector{Point}()
 
-qt = QuadTree(Sqr(400, 400, 400), 2)
+cursor = Sqr(0, 0, 60)
+
+qt = QuadTree(Sqr(WIDTH / 2, HEIGHT / 2, WIDTH), 7)
 
 function draw(v::Game)
     for p in pslol
-        draw(Circle(trunc(Int, p.x), trunc(Int, p.y), 1), colorant"black", fill = true)
+        draw(Circle(trunc(Int, p.x), trunc(Int, p.y), 1), colorant"white", fill = true)
     end
     #draw(Rect(400, 400, 400, 400))
     for sec in generate_quad(qt)
-        draw(Rect(trunc(Int, sec.boundary.x - sec.boundary.s / 2), trunc(Int, sec.boundary.y - sec.boundary.s / 2), trunc(Int, sec.boundary.s), trunc(Int, sec.boundary.s)), colorant"black", fill = false)
+        draw(Rect(trunc(Int, sec.boundary.x - sec.boundary.s / 2), trunc(Int, sec.boundary.y - sec.boundary.s / 2), trunc(Int, sec.boundary.s), trunc(Int, sec.boundary.s)), colorant"white", fill = false)
+    end
+    draw(Circle(trunc(Int, cursor.x), trunc(Int, cursor.y), trunc(Int, cursor.s / 2)), colorant"blue", fill = false)
+    foundp = query(qt, Sqr(cursor.x, cursor.y, 60))
+    for p1 in foundp
+        draw(Circle(trunc(Int, p1.x), trunc(Int, p1.y), 3), colorant"green", fill = true)
     end
 end
 
-function on_mouse_move(v::Game, pos)
+function on_mouse_down(v::Game, pos, button)
     insert!(qt, Point(pos[1], pos[2]))
     push!(pslol, Point(pos[1], pos[2]))
+end
+
+function on_mouse_move(v::Game, pos)
+    cursor.x, cursor.y = pos[1], pos[2]
 end

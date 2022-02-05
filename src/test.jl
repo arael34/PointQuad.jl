04ebff6aab018cee71cdb43c7bc9560893e7cb4d
rushia <jonasg.13452@gmail.com>
2022-02-05 12:31:10 -0500

    cleaning quadtree file

--- src/test.jl
+++ src/test.jl
@@ -1,9 +1,3 @@
-using GameZero
-
-const WIDTH = 800
-const HEIGHT = 800
-const BACKGROUND = colorant"black"
-
 #=
 TODO
 fix and clean up generate_quad()
@@ -34,7 +28,7 @@ end
 
 isleaf(qt::QuadTree) = length(qt.cr) == 0
 
-function vertices(boundary::Sqr)
+function get_subsquares(boundary::Sqr)
     [
         Sqr(boundary.x + boundary.s / 4, boundary.y - boundary.s / 4, boundary.s / 2),
         Sqr(boundary.x - boundary.s / 4, boundary.y - boundary.s / 4, boundary.s / 2),
@@ -44,7 +38,7 @@ function vertices(boundary::Sqr)
 end
 
 function contains(boundary::Sqr, p::Point)
-    return (
+    (
         p.x > boundary.x - boundary.s / 2 &&
         p.x < boundary.x + boundary.s / 2 &&
         p.y > boundary.y - boundary.s / 2 &&
@@ -53,11 +47,11 @@ function contains(boundary::Sqr, p::Point)
 end
 
 function subdivide!(qt::QuadTree)
-    qt.cr = QuadTree.(vertices(qt.boundary), qt.capacity)
+    qt.cr = QuadTree.(get_subsquares(qt.boundary), qt.capacity)
 end
 
 function insert!(qt::QuadTree, p::Point)
-    if !contains(qt.boundary, p) return end
+    if !contains(qt.boundary, p) return nothing end
     if length(qt.points) < qt.capacity
         push!(qt.points, p)
     else
@@ -90,50 +84,17 @@ function query(qt::QuadTree, bounds::Sqr)
     for section in qt.cr
         append!(found, query(section, bounds))
     end
+    
     return found
 end
 
-function generate_quad(qt::QuadTree)
-    l = Vector{QuadTree}()
-    push!(l, qt)
-    if length(qt.points) == 0
-    else
-        for section in qt.cr
-            for quad in generate_quad(section)
-                push!(l, quad)
-            end
-        end
+function generate_trees(qt::QuadTree)
+    trees = Vector{QuadTree}()
+    push!(trees, qt)
+    if length(qt.points) == 0 return trees end
+    for section in qt.cr
+        append!(trees, generate_trees(section))
     end
-    return l
-end
 
-pslol = Vector{Point}()
-foundp = Vector{Point}()
-
-cursor = Sqr(0, 0, 60)
-
-qt = QuadTree(Sqr(WIDTH / 2, HEIGHT / 2, WIDTH), 7)
-
-function draw(v::Game)
-    for p in pslol
-        draw(Circle(trunc(Int, p.x), trunc(Int, p.y), 1), colorant"white", fill = true)
-    end
-    #draw(Rect(400, 400, 400, 400))
-    for sec in generate_quad(qt)
-        draw(Rect(trunc(Int, sec.boundary.x - sec.boundary.s / 2), trunc(Int, sec.boundary.y - sec.boundary.s / 2), trunc(Int, sec.boundary.s), trunc(Int, sec.boundary.s)), colorant"white", fill = false)
-    end
-    draw(Circle(trunc(Int, cursor.x), trunc(Int, cursor.y), trunc(Int, cursor.s / 2)), colorant"blue", fill = false)
-    foundp = query(qt, Sqr(cursor.x, cursor.y, 60))
-    for p1 in foundp
-        draw(Circle(trunc(Int, p1.x), trunc(Int, p1.y), 3), colorant"green", fill = true)
-    end
-end
-
-function on_mouse_down(v::Game, pos, button)
-    insert!(qt, Point(pos[1], pos[2]))
-    push!(pslol, Point(pos[1], pos[2]))
-end
-
-function on_mouse_move(v::Game, pos)
-    cursor.x, cursor.y = pos[1], pos[2]
+    return trees
 end

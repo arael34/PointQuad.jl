b85b503e29f2f726ca9a7f18a8a87ee3a2a446d3
rushia <jonasg.13452@gmail.com>
2022-02-16 16:39:39 -0500

    fixed up bugs

--- src/visualization.jl
+++ src/visualization.jl
@@ -13,14 +13,14 @@ end
 
 qt = PointQuad.QuadTree{P}(PointQuad.Sqr(WIDTH / 2, HEIGHT / 2, WIDTH), 2)
 
-PointQuad.position(p::P) = (p.x, p.y)
+PointQuad.position(p::P) = (trunc(Int, p.x), trunc(Int, p.y))
 
 function draw()
     for p in PointQuad.query(qt, PointQuad.Sqr(WIDTH/2, HEIGHT/2, WIDTH))
         if length(PointQuad.query(qt, PointQuad.Sqr(p.x, p.y, 12))) > 1
-            draw(Circle(trunc(Int, p.x), trunc(Int, p.y), 3), colorant"green", fill = true)
+            draw(Circle(trunc(Int, p.x), trunc(Int, p.y), 0), colorant"green", fill = true)
         else
-            draw(Circle(trunc(Int, p.x), trunc(Int, p.y), 3), colorant"white", fill = true)
+            draw(Circle(trunc(Int, p.x), trunc(Int, p.y), 0), colorant"white", fill = true)
         end
     end
     for sec in PointQuad.get_trees(qt)
@@ -32,13 +32,20 @@ function update()
     agents = PointQuad.query(qt, PointQuad.Sqr(WIDTH/2, HEIGHT/2, WIDTH))
     PointQuad.clear!(qt)
     for p in agents
-        p.x -= 1
+        new_x, new_y = p.x + cos(p.angle), p.y + sin(p.angle)
+        if new_x < 5 || new_x >= WIDTH - 5 || new_y < 5 || new_y >= HEIGHT - 5
+            new_x = min(WIDTH - 5, max(5, new_x))
+            new_y = min(HEIGHT - 5, max(5, new_y))
+            p.angle = rand() * 2 * π
+        end
+        p.x, p.y = new_x, new_y
         PointQuad.insert!(qt, p)
     end
+    agents = nothing
 end
 
 function on_mouse_down(v::Game, pos, button)
-    PointQuad.insert!(qt, P(pos[1], pos[2], 0))
+    PointQuad.insert!(qt, P(pos[1], pos[2], rand() * 2 * π))
     if button == MouseButtons.RIGHT
         PointQuad.clear!(qt)
     end

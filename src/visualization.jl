045cf89277e77a1e91b7edf1f84d7dc7b5c49733
rushia <jonasg.13452@gmail.com>
2022-02-16 19:08:04 -0500

    Update visualization.jl

--- src/visualization.jl
+++ src/visualization.jl
@@ -11,27 +11,19 @@ mutable struct P
     angle::Real
 end
 
-qt = PointQuad.QuadTree{P}(PointQuad.Sqr(WIDTH / 2, HEIGHT / 2, WIDTH), 2)
+qt = PointQuad.QuadTree{P}(PointQuad.Sqr(WIDTH / 2, HEIGHT / 2, WIDTH), 4)
 
 PointQuad.position(p::P) = (trunc(Int, p.x), trunc(Int, p.y))
 
 function draw()
-    for p in PointQuad.query(qt, PointQuad.Sqr(WIDTH/2, HEIGHT/2, WIDTH))
+    agents = PointQuad.query(qt, PointQuad.Sqr(WIDTH/2, HEIGHT/2, WIDTH))
+    PointQuad.clear!(qt)
+    for p in agents
         if length(PointQuad.query(qt, PointQuad.Sqr(p.x, p.y, 12))) > 1
             draw(Circle(trunc(Int, p.x), trunc(Int, p.y), 0), colorant"green", fill = true)
         else
             draw(Circle(trunc(Int, p.x), trunc(Int, p.y), 0), colorant"white", fill = true)
         end
-    end
-    for sec in PointQuad.get_trees(qt)
-        draw(Rect(trunc(Int, sec.boundary.x - sec.boundary.s / 2), trunc(Int, sec.boundary.y - sec.boundary.s / 2), trunc(Int, sec.boundary.s), trunc(Int, sec.boundary.s)), colorant"white", fill = false)
-    end
-end
-
-function update()
-    agents = PointQuad.query(qt, PointQuad.Sqr(WIDTH/2, HEIGHT/2, WIDTH))
-    PointQuad.clear!(qt)
-    for p in agents
         new_x, new_y = p.x + cos(p.angle), p.y + sin(p.angle)
         if new_x < 5 || new_x >= WIDTH - 5 || new_y < 5 || new_y >= HEIGHT - 5
             new_x = min(WIDTH - 5, max(5, new_x))
@@ -42,11 +34,11 @@ function update()
         PointQuad.insert!(qt, p)
     end
     agents = nothing
+    for sec in PointQuad.get_trees(qt)
+        draw(Rect(trunc(Int, sec.boundary.x - sec.boundary.s / 2), trunc(Int, sec.boundary.y - sec.boundary.s / 2), trunc(Int, sec.boundary.s), trunc(Int, sec.boundary.s)), colorant"white", fill = false)
+    end
 end
 
-function on_mouse_down(v::Game, pos, button)
+function on_mouse_move(g::Game, pos)
     PointQuad.insert!(qt, P(pos[1], pos[2], rand() * 2 * π))
-    if button == MouseButtons.RIGHT
-        PointQuad.clear!(qt)
-    end
 end
